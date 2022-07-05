b3969780faca636a0f3d5b370c6c8cf287efc27a
KenjiWorkES <lucas.kenjies@gmail.com>
2022-07-05 15:59:36 -0300

    add modifier homeHeader__pokeball--right and left

--- src/components/atoms/HomeLink/index.jsx
+++ src/components/atoms/HomeLink/index.jsx
@@ -3,6 +3,10 @@ import "./homeLink.scss";
 const HomeLink = ({ text, color }) => {
   return (
     <li className="homeHeader__item">
+      <img
+        className="homeHeader__pokeball homeHeader__pokeball--right"
+        src="/assets/pokeball-right-icon.svg"
+      />
       <a className="homeHeader__link">{text}</a>
     </li>
   );
