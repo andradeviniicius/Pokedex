d57e6a2e8d714023a60deea403e964f112600c6b
andradeviniicius <viniciusdandrade01@gmail.com>
2022-07-05 17:09:09 -0300

    Removing remporary HomeScreen page used for test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import PokeNews from "@organisms/PokeNews";
-import HomeScreen from "./components/pages/HomeScreen";
 import { HomePage } from "./components/pages";
 
 function App() {
   return (
     <>
-      <HomeScreen/>
       <HomePage />
     </>
   );
