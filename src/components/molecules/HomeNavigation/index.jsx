561a0ea9062526847b3bc5f36f8b9c369311d9c7
KenjiWorkES <lucas.kenjies@gmail.com>
2022-07-05 16:40:58 -0300

    add background colors to homeHeader__item modifiers

--- src/components/molecules/HomeNavigation/index.jsx
+++ src/components/molecules/HomeNavigation/index.jsx
@@ -5,12 +5,12 @@ const HomeNavigation = () => {
   return (
     <nav className="homeHeader__nav">
       <ul className="homeHeader__list">
-        <HomeLink text="Pokedex" />
-        <HomeLink text="Pokedex" />
-        <HomeLink text="Pokedex" />
-        <HomeLink text="Pokedex" />
-        <HomeLink text="Pokedex" />
-        <HomeLink text="Pokedex" />
+        <HomeLink text="Pokedex" color="green" />
+        <HomeLink text="Moves" color="red" />
+        <HomeLink text="Abilities" color="blue" />
+        <HomeLink text="Items" color="yellow" />
+        <HomeLink text="Locations" color="purple" />
+        <HomeLink text="Type Charts" color="brown" />
       </ul>
     </nav>
   );
