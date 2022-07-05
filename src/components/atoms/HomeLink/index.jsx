a57fe7921d64cd11fa9cdf49f73e0f839f193ccc
KenjiWorkES <lucas.kenjies@gmail.com>
2022-07-05 15:40:56 -0300

    add homeHeader__link and homeHeader_item scc

--- src/components/atoms/HomeLink/index.jsx
+++ src/components/atoms/HomeLink/index.jsx
@@ -1,7 +1,11 @@
 import "./homeLink.scss";
 
 const HomeLink = ({ text, color }) => {
-  return <li className="homeHeader__item">{text}</li>;
+  return (
+    <li className="homeHeader__item">
+      <a className="homeHeader__link">{text}</a>
+    </li>
+  );
 };
 
 export default HomeLink;
