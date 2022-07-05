d075a5abe820f3e45d099a762d056c8172f322da
andradeviniicius <viniciusdandrade01@gmail.com>
2022-07-05 15:57:05 -0300

    Creating Alias for atoms molecules and organisms

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@atoms": path.resolve(__dirname, "./src/components/atoms"),
+      "@molecules": path.resolve(__dirname, "./src/components/molecules"),
+      "@organisms": path.resolve(__dirname, "./src/components/organisms"),
     },
   },
   plugins: [react()],
