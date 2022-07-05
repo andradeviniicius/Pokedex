e15b400cdff259c254cdcf69d6b5802cc018a5f6
KenjiWorkES <lucas.kenjies@gmail.com>
2022-07-05 14:41:38 -0300

    created SearchInput jsx structure

--- src/components/atoms/SearchInput/index.jsx
+++ src/components/atoms/SearchInput/index.jsx
@@ -1,3 +1,12 @@
-const SearchInput = () => {};
+const SearchInput = () => {
+  return (
+    <div>
+      <button>
+        <img src="" alt="Search Button" />
+      </button>
+      <input></input>
+    </div>
+  );
+};
 
 export default SearchInput;
