7dbf0d9d9cc29afd2cc4e0026a4e350ac8f248be
KenjiWorkES <lucas.kenjies@gmail.com>
2022-07-05 15:18:27 -0300

    add jsx to HomeNavigation and Homelink and created the scss files

--- src/components/atoms/HomeLink/index.jsx
+++ src/components/atoms/HomeLink/index.jsx
@@ -1,3 +1,7 @@
-const HomeLink = () => {};
+import "./homeLink.scss";
+
+const HomeLink = ({ text, color }) => {
+  return <li className="homeHeader__item"></li>;
+};
 
 export default HomeLink;
--- src/components/molecules/HomeNavigation/index.jsx
+++ src/components/molecules/HomeNavigation/index.jsx
@@ -1,3 +1,14 @@
-const HomeNavigation = () => {};
+import { HomeLink } from "@/components/atoms";
+import "./homeNavigation.scss";
+
+const HomeNavigation = () => {
+  return (
+    <nav className="homeHeader__nav">
+      <ul className="homeHeader__list">
+        <HomeLink />
+      </ul>
+    </nav>
+  );
+};
 
 export default HomeNavigation;
