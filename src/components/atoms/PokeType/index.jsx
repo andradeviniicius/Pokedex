73743326df51bba4466273c915415fc2394f20c7
andradeviniicius <viniciusdandrade01@gmail.com>
2022-07-06 18:40:00 -0300

    Poketype initial structure

---
+++ src/components/atoms/PokeType/index.jsx
@@ -0,0 +1,13 @@
+import "./pokeType.scss";
+
+// It receives the type of the pokemon from the component that uses him
+// or from redux cause we're in the first sprint and not everything is clear yet
+export default function PokeType({ type }) {
+  return (
+    <>
+      <div className="pokeType">
+        <p>{type}</p>
+      </div>
+    </>
+  );
+}
