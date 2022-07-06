8064e6f1c175d0d431da8bd694c60f5aef3068f7
andradeviniicius <viniciusdandrade01@gmail.com>
2022-07-06 12:09:08 -0300

    PokeBurguer > initial structure for the burguer

--- src/components/atoms/PokeBurguer/index.jsx
+++ src/components/atoms/PokeBurguer/index.jsx
@@ -1,9 +1,11 @@
+import './pokeBurguer.scss'
+
 export default function PokeBurguer() {
   return (
-    <div className="pokeBurguer">
+    <button className="pokeBurguer">
       <div className="pokeBurguer__line1"></div>
       <div className="pokeBurguer__line2"></div>
       <div className="pokeBurguer__line3"></div>
-    </div>
+    </button>
   );
 }
