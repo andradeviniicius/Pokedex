2acb2b2660e0270d503f10da7130cff7dfb71370
andradeviniicius <viniciusdandrade01@gmail.com>
2022-07-08 10:19:04 -0300

    feature/ pokeNewsHead desktop/tablet responsive

--- src/components/atoms/PokeNewsItem/index.jsx
+++ src/components/atoms/PokeNewsItem/index.jsx
@@ -3,14 +3,19 @@ import pokeRush from "@assets/article01.png";
 
 export default function PokeNewsItem() {
   return (
-    <div className="pokeNewsItem">
-      <div className="pokeNewsItem__leftContainer">
-        <p className="pokeNewsItem__text">Pokémon Rumblee Rush Arrives Soon</p>
-        <p className="pokeNewsItem__date">15 May 2019</p>
+    <>
+      <div className="pokeNewsItem">
+        <div className="pokeNewsItem__leftContainer">
+          <p className="pokeNewsItem__text">
+            Pokémon Rumblee Rush Arrives Soon
+          </p>
+          <p className="pokeNewsItem__date">15 May 2019</p>
+        </div>
+        <div className="pokeNewsItem__rightContainer">
+          <img className="pokeNewsItem__image" src={pokeRush} alt="" />
+        </div>
       </div>
-      <div className="pokeNewsItem__rightContainer">
-        <img className="pokeNewsItem__image" src={pokeRush} alt="" />
-      </div>
-    </div>
+      <hr className="pokeNewsItem__cardSeparator" />
+    </>
   );
 }
