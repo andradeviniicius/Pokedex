8eae842826dc07acf6588c06b95bceb0de648f5a
andradeviniicius <viniciusdandrade01@gmail.com>
2022-07-08 16:33:12 -0300

    feature/pokedexItem defaultID#000 &1°letterCapital

--- src/components/atoms/PokedexItem/index.jsx
+++ src/components/atoms/PokedexItem/index.jsx
@@ -1,4 +1,5 @@
 import { PokeType } from "@atoms";
+import { flushSync } from "react-dom";
 
 import { Link } from "react-router-dom";
 
@@ -6,15 +7,21 @@ import "./pokedexItem.scss";
 import "./pokedexItemModifiers.scss";
 
 export default function PokedexItem({ pokeName, pokeImage, pokeType, pokeId }) {
+  function addExtraZero(num, totalLenght) {
+    return String(num).padStart(totalLenght, "0");
+  }
+
   return (
     <>
       <Link to={"/pokedex"}>
         <li
           className={`pokedex__item ${`pokedex__item--${pokeType[0].type.name.toLowerCase()}`}`}
         >
-          <p className="pokedex__pokeId">#{pokeId}</p>
+          <p className="pokedex__pokeId">#{addExtraZero(pokeId, 3)}</p>
 
-          <p className="pokedex__pokeName">{pokeName}</p>
+          <p className="pokedex__pokeName">
+            {pokeName.charAt(0).toUpperCase() + pokeName.slice(1)}
+          </p>
           <div className="pokedex__pokeTypeContainer">
             {pokeType.map((data) => {
               return <PokeType type={data.type.name} />;
