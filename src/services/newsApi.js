6b615f6c9efbf82dda5caf032777dd9846470d92
andradeviniicius <viniciusdandrade01@gmail.com>
2022-07-10 19:22:41 -0300

    feature/ search only us-en & title include Pokemon

--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -1,6 +1,8 @@
 const url =
   "https://newsapi.org/v2/everything?" +
   "q=Pokemon&" +
+  "searchIn=title&"+
+  "language=en&" +
   "from=2022-07-08&" +
   "sortBy=popularity&" +
   "apiKey=a859eb66cf3541afa25cf257483c9300";
@@ -9,10 +11,10 @@ export async function fetchPokeNews() {
   const res = await fetch(url);
 
   if (!res.ok) {
+    console.log(res);
     throw new Error("error!");
   }
 
   const data = res.json();
-  console.log(data);
   return data;
 }
