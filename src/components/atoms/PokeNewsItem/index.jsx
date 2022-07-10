1b5be51bd0507a8920b69e3c4bc5e9a1372f273c
andradeviniicius <viniciusdandrade01@gmail.com>
2022-07-10 20:08:00 -0300

    fix/ fixing date format

--- src/components/atoms/PokeNewsItem/index.jsx
+++ src/components/atoms/PokeNewsItem/index.jsx
@@ -1,12 +1,14 @@
 import "./pokeNewsItem.scss";
 
 export default function PokeNewsItem({ title, date, image }) {
+  const javascriptDate = new Date(date).toLocaleDateString('en-gb', { year: 'numeric', month: 'long', day: '2-digit' })
+
   return (
     <>
       <div className="pokeNewsItem">
         <div className="pokeNewsItem__leftContainer">
           <p className="pokeNewsItem__text">{title}</p>
-          <p className="pokeNewsItem__date">{date}</p>
+          <p className="pokeNewsItem__date">{javascriptDate}</p>
         </div>
         <div className="pokeNewsItem__rightContainer">
           <img className="pokeNewsItem__image" src={image} alt="" />
