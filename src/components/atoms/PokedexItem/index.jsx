3b48f59e116a821fa6049d50124908a2bc9aea7e
andradeviniicius <viniciusdandrade01@gmail.com>
2022-07-12 18:24:11 -0300

    fix/ initial route for pokeDexItems(Pokemon cards)

--- src/components/atoms/PokedexItem/index.jsx
+++ src/components/atoms/PokedexItem/index.jsx
@@ -1,12 +1,13 @@
 import { PokeType } from "@atoms";
 import { flushSync } from "react-dom";
 
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import "./pokedexItem.scss";
 import "./pokedexItemModifiers.scss";
 
 export default function PokedexItem({ pokeName, pokeImage, pokeType, pokeId }) {
+  const navigate = useNavigate();
   function addExtraZero(num, totalLenght) {
     return String(num).padStart(totalLenght, "0");
   }
@@ -15,12 +16,16 @@ export default function PokedexItem({ pokeName, pokeImage, pokeType, pokeId }) {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
 
+  function changePages() {
+    navigate(`/pokedex/aboutPokemon/${pokeId}`);
+  }
+
   return (
     <>
       <li
         className={`pokedex__item ${`pokedex__item--${pokeType[0].type.name.toLowerCase()}`}`}
       >
-        <Link to={"/pokedex"}>
+        <div onClick={changePages}>
           <p className="pokedex__pokeId">#{addExtraZero(pokeId, 3)}</p>
 
           <p className="pokedex__pokeName">{capitalFirstLetter(pokeName)}</p>
@@ -36,7 +41,7 @@ export default function PokedexItem({ pokeName, pokeImage, pokeType, pokeId }) {
             src={"./../../../../public/assets/pokedex-item-bg.svg"}
             alt=""
           />
-        </Link>
+        </div>
       </li>
     </>
   );
