32680fe5f0e4dca8b2c71d26c7605c770ae53804
KenjiWorkES <lucas.kenjies@gmail.com>
2022-07-14 11:30:00 -0300

    feature/ add NavLink to PokeLink and the activeClass

--- src/components/atoms/PokeLink/index.jsx
+++ src/components/atoms/PokeLink/index.jsx
@@ -1,5 +1,15 @@
+import "./pokeLink.scss";
+import { NavLink } from "react-router-dom";
+
 const PokeLink = ({ text }) => {
-  return <li className="pokeDetail__link">{text}</li>;
+  return (
+    <NavLink
+      to="/detailPokemon"
+      className={(state) => (state.isActive ? "pokeDetail__link--active" : "")}
+    >
+      <li className="pokeDetail__link">{text}</li>
+    </NavLink>
+  );
 };
 
 export default PokeLink;
