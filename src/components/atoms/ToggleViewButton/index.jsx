49ddfef471428ca0164a9f6a9164f55fe88d665f
andradeviniicius <viniciusdandrade01@gmail.com>
2022-07-14 10:53:33 -0300

    refac/ temporary toggleViewModeButton

--- src/components/atoms/ToggleViewButton/index.jsx
+++ src/components/atoms/ToggleViewButton/index.jsx
@@ -1,11 +1,13 @@
-import './toggleViewButton.scss'
+import "./toggleViewButton.scss";
+import tempLogo from "@assets/tempToggleViewButton.svg";
 
 export default function ToggleViewButton() {
   return (
     <button className="toggleViewButton">
-      <span className="toggleViewButton__line1"></span>
+      <img className="toggleViewButton__tempLogo" src={tempLogo} alt="" />
+      {/* <span className="toggleViewButton__line1"></span>
       <span className="toggleViewButton__line2"></span>
-      <span className="toggleViewButton__line3"></span>
+      <span className="toggleViewButton__line3"></span> */}
     </button>
   );
 }
