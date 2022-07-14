96f2a7c86559813eafd55d8f8e1f5c96ae6b77f3
andradeviniicius <viniciusdandrade01@gmail.com>
2022-07-14 10:42:15 -0300

    refac/ using spans for best practices

--- src/components/atoms/PokeBurguer/index.jsx
+++ src/components/atoms/PokeBurguer/index.jsx
@@ -3,9 +3,9 @@ import './pokeBurguer.scss'
 export default function PokeBurguer() {
   return (
     <button className="pokeBurguer">
-      <div className="pokeBurguer__line1"></div>
-      <div className="pokeBurguer__line2"></div>
-      <div className="pokeBurguer__line3"></div>
+      <span className="pokeBurguer__line1"></span>
+      <span className="pokeBurguer__line2"></span>
+      <span className="pokeBurguer__line3"></span>
     </button>
   );
 }
