a6781aadeaa8836c7bae1c6b44d19d328b70edf5
andradeviniicius <viniciusdandrade01@gmail.com>
2022-07-15 10:11:03 -0300

    refac/adding extra style for loadding

--- src/components/atoms/LoadingSpinner/index.jsx
+++ src/components/atoms/LoadingSpinner/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import "./loadingSpinner.scss";
 
 const LoadingSpinner = React.forwardRef((props, ref) => {
-  return <div className="spinner" ref={ref}></div>;
+  return <div className={`spinner ${props.extraClasses}`} ref={ref}></div>;
 });
 
 export default LoadingSpinner;
